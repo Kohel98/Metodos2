8c0788fe55154db84261aa2b4424ab76b7599b56
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-05-14 23:04:22 -0600

    Update Main.py
    14/05/2023-solo_teoria

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
-import sympy as sy
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.header(":blue[M´etodo de Newton]")
+"""
+El método de Newton es un método iterativo para encontrar raíces de una función no lineal,
+y también se puede utilizar para resolver sistemas de ecuaciones no lineales. 
+El método se basa en la aproximación de la función no lineal en cada iteración mediante una función lineal, 
+y luego se encuentra la raíz de la función lineal mediante el método de resolución de sistemas de ecuaciones lineales.
+"""
