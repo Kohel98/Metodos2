caae65e377c041d2394e6bfaa4751f229c2c4bd7
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-06-01 00:34:28 -0600

    aver

--- pages/TestDG_lotes.py
+++ pages/TestDG_lotes.py
@@ -5,8 +5,7 @@ import seaborn as sns
 import time
 import streamlit as st
 from scipy import stats 
-from sklearn
-
+import sklearn
 def timeit(method):
     def timed(*args, **kw):
         ts = time.time()
