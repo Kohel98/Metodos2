3619d1d8e245ecb4aef39b5dae7bea1352e2f044
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-06-01 08:08:04 -0600

    a ver

--- pages/Descenso de gradiente.py
+++ pages/Descenso de gradiente.py
@@ -4,14 +4,61 @@ import pandas as pd
 import functions.sfunctions as sf
 
 
-tab1, tab2, tab3 = st.tabs(["Definiciones","Ejemplos","Aplicaciones"])
+tab1, tab2, tab3 = st.tabs(["Definiciones","Aplicaciones"])
+
+
+with tab1:
+     st.title(":blue[Descenso de la Gradiente]")
+     """
+         Descenso de gradiente es conocido como uno de los algoritmos de optimización más utilizados
+         para entrenar modelos de aprendizaje automático mediante la minimización de errores 
+         entre los resultados reales y esperados.
+         
+         Además, el descenso de gradiente también se usa para entrenar redes neuronales.
+         
+         El :red[**objetivo principal**] del descenso de gradiente es minimizar la función convexa 
+         mediante la iteración de actualizaciones de parámetros. 
+         
+         Una vez que se optimizan estos modelos de aprendizaje automático,
+         estos modelos se pueden usar como herramientas poderosas para la inteligencia artificial 
+         y varias aplicaciones informáticas.
+         
+         Este se define como uno de los algoritmos de optimización iterativos de aprendizaje automático 
+         más utilizados para entrenar los modelos de aprendizaje automático y aprendizaje profundo.
+         Ayuda a encontrar el mínimo local de una función.
+         La mejor manera de definir el mínimo local o el máximo local de una función mediante el descenso de gradiente es la siguiente: 
+         + Si nos movemos hacia un gradiente negativo o nos alejamos del gradiente de la función en el punto actual, dará el :red[**mínimo local**] de esa función.
+         + Siempre que nos desplacemos hacia un gradiente positivo o hacia el gradiente de la función en el punto actual, obtendremos el :red[**máximo local**] de esa función.
+            
+         Su procedimiemto consiste en  usar un algoritmo de descenso de gradiente 
+         en minimizar la función de costo usando la iteración. 
+         Para lograr este objetivo, realiza dos pasos iterativamente:
+         + Calcula la derivada de primer orden de la función para calcular el gradiente o pendiente de esa función.
+         + Alejarse de la dirección del gradiente, lo que significa que la pendiente aumentó desde el punto actual en alfa veces, donde Alfa se define como Tasa de aprendizaje. 
+         
+         Es un parámetro de ajuste en el proceso de optimización que ayuda a decidir el lapso del tiempo de los pasos. Un ejemplo seria cuando se grafica una gradiente negativa (pasando las coordenadas del nuevo punto) con un proceso iterativo hasta encontrar el minimo local.
+         Estos pasos se denomina como **taza de aprendizaje**. 
+         
+         Con una tasa de aprendizaje alta podemos cubrir más terreno en cada paso, pero corremos el riesgo de 
+         sobrepasar el punto más bajo ya que la pendiente de la colina cambia constantemente.
+         Con una tasa de aprendizaje muy baja, podemos movernos con confianza en la dirección del gradiente 
+         negativo ya que lo estamos recalculando con tanta frecuencia. Una tasa de aprendizaje baja es más precisa, 
+         pero calcular el gradiente requiere mucho tiempo, por lo que nos llevará mucho tiempo llegar al fondo.
+                 
+        :red[**Función de costo**] 
+        Con esta por medio de la función de pérdida determinamos "Qué tan bueno" es nuestro modelo para hacer 
+        predicciones para un conjunto dado de parámetros. La función de costo tiene su proria curva y sus gradientes.
+        La pendiente de esta curvatura nos mostrara cómo actualizar nuestros parámetros para que el modelo sea más preciso.
+                
+         """
+         
 
 
 
 
 
 
-with tab3:
+with tab2:
     # pylint: disable=line-too-long
     def write():
         #"""Used to write the page in the app.py file"""
@@ -19,50 +66,37 @@ with tab3:
             #ast.shared.components.title_awesome(" - Homepage")
             #st.title("LiRA Web App - HomePage")
             
-            st.text('''
+            st.write('''
                     Descenso de la gradiente 
-                    el proposito es simular un descenso de una gradiente''')
+                    el proposito es simular un descenso de una gradiente 
                     
         
-          
-            st.subtitle(''' 1. Simular la funcion lineal''')
-            """
+            ####
+            ## 1. Simular la funcion lineal
+            
             Normalmente, durante la regresión lineal, intentamos predecir una función lineal del tipo
             Y = aX + b a partir de las muestras de datos. En este caso no hay conjunto de datos, por lo que vamos a crear una predefinida y configurable
             predefinida y configurable para generar estos datos. Para ello necesitamos especificar lo siguiente:
-            """
-            st.text(''':red[Distribución de x (Input)]''')
-            """
+            
+            #### Distribución de x (Input)
             Para generar muestras de datos aleatorios para la entrada X, necesitaremos conocer la Media y la Desviación de la distribución,
             que pueden establecerse ajustando los controles respectivos en el widget de la barra lateral.
             
-            """
+            #### Coeficientes (a, b)
+            La Pendiente y el Intercepto de la función lineal también pueden ajustarse utilizando los controles disponibles en el widget de la barra lateral.
             
-            st.text(''':red[Coeficientes (a, b)]''')
-            """
-             La Pendiente y el Intercepto de la función lineal también pueden ajustarse utilizando los controles disponibles en el widget de la barra lateral.
-            
-        """
-           
-         
-            st.subtitle(''' 2. Generar dato aleatorio de poblacion''') 
-            '''
-             Una vez que hemos simulado una función lineal, necesitamos infundir estos datos con algo de ruido, para permitir que el algoritmo descubra
+            ####
+            ## 2. Generar dato aleatorio de poblacion
+        Una vez que hemos simulado una función lineal, necesitamos infundir estos datos con algo de ruido, para permitir que el algoritmo descubra
             la función lineal simulada. Para ello debemos especificar el número de muestras "n" y la media de la distribución del error
             o Media del Error (Residual):
                 
-            '''
-       
-            st.text(''':red[Numero de Muestras - n]''')
-            '''
+            #### Numero de Muestras - n
             Para generar los datos, es necesario especificar el número de puntos de datos según el control correspondiente de la barra lateral.
             
-            '''
-            
-            st.text(''':red[Residual - e]''')#### 
-            '''
+            #### Residual - e
         Distribución del error que se añadirá a Y para generar las Muestras_Y.      
-            '''
+            ''')
             
             # ****************************************************************************************************
             # Input widgets
